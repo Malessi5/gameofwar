5d456c3e044ddfe40eb763564283342c717f4d8d
malessi5 <mikealessi5@gmail.com>
2021-10-20 17:44:03 -0400

    try again

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,10 @@ const Sequelize = require("sequelize");
 
 const db = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost:5432/war",
-  {logging: false, dialectOptions: {ssl: true}}
+  {
+    logging: false,
+    dialectOptions: {ssl: {require: true, rejectUnauthorized: false}},
+  }
 );
 
 module.exports = db;
